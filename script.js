889dc47bfb502deb4d6591e0ac9dcfe87bfbab98
FRJohan312 <johan37021@gmail.com>
2025-02-21 17:23:15 -0500

    corrección de errores

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-$("#messageState").on("change", (x) => {
-	$(".message").removeClass("openNor").removeClass("closeNor");
-	if ($("#messageState").is(":checked")) {
-		$(".message").removeClass("closed").removeClass("no-anim").addClass("openNor");
-		$(".heart").removeClass("closeHer").removeClass("openedHer").addClass("openHer");
-		$(".container").stop().animate({"backgroundColor": "#f48fb1"}, 2000);
+$j("#messageState").on("change", (x) => {
+	$j(".message").removeClass("openNor").removeClass("closeNor");
+	if ($j("#messageState").is(":checked")) {
+		$j(".message").removeClass("closed").removeClass("no-anim").addClass("openNor");
+		$j(".heart").removeClass("closeHer").removeClass("openedHer").addClass("openHer");
+		$j(".container").stop().animate({"backgroundColor": "#f48fb1"}, 2000);
 		console.log("Abrindo");
 	} else {
-		$(".message").removeClass("no-anim").addClass("closeNor");
-		$(".heart").removeClass("openHer").removeClass("openedHer").addClass("closeHer");
-		$(".container").stop().animate({"backgroundColor": "#fce4ec"}, 2000);
+		$j(".message").removeClass("no-anim").addClass("closeNor");
+		$j(".heart").removeClass("openHer").removeClass("openedHer").addClass("closeHer");
+		$j(".container").stop().animate({"backgroundColor": "#fce4ec"}, 2000);
 		console.log("fechando");
 	}
 });
 
-$(".message").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+$j(".message").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
 	console.log("Animation End");
-	if ($(".message").hasClass("closeNor"))
-		$(".message").addClass("closed");
-	$(".message").removeClass("openNor").removeClass("closeNor").addClass("no-anim");
+	if ($j(".message").hasClass("closeNor"))
+		$j(".message").addClass("closed");
+	$j(".message").removeClass("openNor").removeClass("closeNor").addClass("no-anim");
 	
 });
 
-$(".heart").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+$j(".heart").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
 	console.log("Animation End");
-	if (!$(".heart").hasClass("closeHer"))
-		$(".heart").addClass("openedHer").addClass("beating");
+	if (!$j(".heart").hasClass("closeHer"))
+		$j(".heart").addClass("openedHer").addClass("beating");
 	else
-		$(".heart").addClass("no-anim").removeClass("beating");
-	$(".heart").removeClass("openHer").removeClass("closeHer");
+		$j(".heart").addClass("no-anim").removeClass("beating");
+	$j(".heart").removeClass("openHer").removeClass("closeHer");
 
 });
 
-$(document).ready(function () {
-    const heartsContainer = $("<div class='hearts-container'></div>");
-    $(".container").append(heartsContainer);
+$j(document).ready(function () {
+    const heartsContainer = $j("<div class='hearts-container'></div>");
+    $j(".container").append(heartsContainer);
 
     let interval;
 
-    $("#messageState").on("change", function () {
-        if ($(this).is(":checked")) {
+    $j("#messageState").on("change", function () {
+        if ($j(this).is(":checked")) {
             startHeartsAnimation();
         } else {
             stopHeartsAnimation();
@@ -47,7 +47,7 @@ $(document).ready(function () {
 
     function startHeartsAnimation() {
         interval = setInterval(() => {
-            let heart = $("<span class='floating-heart'>❤️</span>");
+            let heart = $j("<span class='floating-heart'>❤️</span>");
             let randomSize = Math.random() * 15 + 10; // Tamaño aleatorio entre 10px y 25px
             heart.css({
                 left: Math.random() * 100 + "vw",
@@ -58,14 +58,14 @@ $(document).ready(function () {
             heartsContainer.append(heart);
 
             heart.on("animationend", function () {
-                $(this).remove(); // Elimina el corazón al salir de la pantalla
+                $j(this).remove(); // Elimina el corazón al salir de la pantalla
             });
         }, 500); // Aparece un corazón cada 500ms
     }
 
     function stopHeartsAnimation() {
         clearInterval(interval);
-        $(".floating-heart").remove(); // Elimina todos los corazones cuando se cierra la carta
+        $j(".floating-heart").remove(); // Elimina todos los corazones cuando se cierra la carta
     }
 });
 
